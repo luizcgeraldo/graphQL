305d751f69bc6f329dc48156ee604b1bbd01c57f
luizcgeraldo <luizclaudiogeraldo@gmail.com>
2023-08-22 21:05:00 -0300

    first commit

---
+++ cmd/server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	database "command-line-arguments/home/luiz/Projects/fullcycle2/integracao/graphql/internal/database/category.go"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/luizcgeraldo/devfullcycle/13-GraphQL/graph"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open connection to sqlite: %v", err)
+	}
+	defer db.close()
+
+	categoryDb := database.NewCategory(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
