940b6e604fc70e8d33eaa10d575cf8b424035a8d
luizcgeraldo <luizclaudiogeraldo@gmail.com>
2023-08-22 21:53:32 -0300

    ajuste

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -13,15 +13,14 @@ import (
 
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
-	category, err := r.CategoryDB.Create(input.Name, input.Description)
+	category, err := r.CategoryDB.Create(input.Name, *input.Description)
 	if err != nil {
 		return nil, err
 	}
-
 	return &model.Category{
 		ID:          category.ID,
 		Name:        category.Name,
-		Description: category.Description,
+		Description: &category.Description,
 	}, nil
 }
 
